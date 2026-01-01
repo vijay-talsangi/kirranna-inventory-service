0daf110671bd305085def4b3886cd8d3614a146b
vijay-talsangi <vijaytalsangi4705@gmail.com>
2026-01-02 00:02:25 +0530

    Added Tests

--- src/test/java/com/talsangi/kirranna/inventory/InventoryServiceApplicationTests.java
+++ src/test/java/com/talsangi/kirranna/inventory/InventoryServiceApplicationTests.java
@@ -1,15 +1,59 @@
 package com.talsangi.kirranna.inventory;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers // Add this
 class InventoryServiceApplicationTests {
 
+	@Container
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldReadInventory() {
+		var response = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=iphone-15&quantity=1") // Passing a URI object is the key
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract()
+				.as(Boolean.class);
+
+		assertTrue(response);
+
+		var negativeResponse = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=iphone-15&quantity=1000")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract()
+				.as(Boolean.class);
+
+		assertFalse(negativeResponse);
 	}
 
 }
